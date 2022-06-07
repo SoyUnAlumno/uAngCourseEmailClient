cab1543410c785a1766f79258e765bd7c3b1f402
SoyUnAlumno <tomaspraga@hotmail.es>
2022-06-07 15:56:36 +0200

    create input component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,13 +5,14 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
-
+import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
   imports: [
     CommonModule, 
     AuthRoutingModule, 
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
 })
 export class AuthModule {}
