e5f5ca0b442ed11c8b18b40eef6252a413a1f043
SoyUnAlumno <tomaspraga@hotmail.es>
2022-06-08 09:45:36 +0200

    handle signup request success

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,20 +2,28 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 export interface UsernameAvailableRespone {
-  available: boolean
+  available: boolean;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-usernameAvailable(username: string) {
- return this.http
-      .post<UsernameAvailableRespone>('https://api.angular-email.com/auth/username', {
+  usernameAvailable(username: string) {
+    return this.http.post<UsernameAvailableRespone>(
+      'https://api.angular-email.com/auth/username',
+      {
         username
-      })
-}
+      }
+    );
+  }
+
+  signup(credentials: any) {
+    return this.http.post<any>(
+      'https://api.angular-email.com/auth/signup',
+      credentials
+    )
+  }
 }
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -2,43 +2,57 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatchPassword } from '../validators/match-password';
 import { UniqueUsername } from '../validators/unique-username';
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
   username: FormControl = new FormControl('');
-  
-authForm = new FormGroup({
-  username : new FormControl('', [
-    Validators.required,
-    Validators.minLength(3),
-    Validators.maxLength(20),
-    Validators.pattern(/^[a-z0-9]+$/)    
-  ], [
-    this.uniqueUsername.validate
-  ]),
-  password: new FormControl('', [
-    Validators.required,
-    Validators.minLength(4),
-    Validators.maxLength(20),
-  ]),
-  passwordConfirmation: new FormControl('',[
-    Validators.required,
-    Validators.minLength(4),
-    Validators.maxLength(20),
-  ])
-}, {
-  validators: [
-    this.matchPassword.validate
-  ]
-});
-  constructor(private matchPassword: MatchPassword,
-    private uniqueUsername: UniqueUsername) { }
 
-  ngOnInit(): void {
+  authForm = new FormGroup(
+    {
+      username: new FormControl(
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+          Validators.pattern(/^[a-z0-9]+$/),
+        ],
+        [this.uniqueUsername.validate]
+      ),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+      passwordConfirmation: new FormControl('', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20),
+      ]),
+    },
+    {
+      validators: [this.matchPassword.validate],
+    }
+  );
+  constructor(
+    private matchPassword: MatchPassword,
+    private uniqueUsername: UniqueUsername,
+    private authService:  AuthService
+  ) {}
 
+  ngOnInit(): void {}
+  onSubmit() {
+    // Returns if Form is invalid
+    if(this.authForm.invalid){
+      return;
+    }
+    this.authService.signup(this.authForm.value).
+    subscribe(((response) => {
+      console.log(response);
+    }));
   }
-
 }
