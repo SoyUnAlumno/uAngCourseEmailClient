05cc3606b1748a107ac772008f6a27ad6d8ed749
SoyUnAlumno <tomaspraga@hotmail.es>
2022-06-08 17:43:31 +0200

    create method to request emails

--- src/app/inbox/email.service.ts
+++ src/app/inbox/email.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface EmailSummary{
+id: string,
+subject: string,
+from: string,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmailService {
+rootUrl: string = 'https://api.angular-email.com';
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+getEmails() {
+  this.http.get<EmailSummary[]>(`${this.rootUrl}/emails`,
+  // No need for credentials because we previousl set that up with the interceptor
+  )
+}
+
 }
