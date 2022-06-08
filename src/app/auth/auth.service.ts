c1ea582e206c384cd78ed0fa68d00b1c8972ea25
SoyUnAlumno <tomaspraga@hotmail.es>
2022-06-08 12:11:53 +0200

    keep the cookies to identify user

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,59 +2,54 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, tap } from 'rxjs';
 
-
 export interface UsernameAvailableRespone {
   available: boolean;
 }
 interface SignupCredentials {
-  username: string,
-  password: string,
-  passwordConfirmation: string,
-  
+  username: string;
+  password: string;
+  passwordConfirmation: string;
 }
 
 interface SignupResponse {
- username: string
+  username: string;
 }
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  rootUrl = 'https://api.angular-email.com'
+  rootUrl = 'https://api.angular-email.com';
   signedin$ = new BehaviorSubject(false);
-
 
   constructor(private http: HttpClient) {}
   usernameAvailable(username: string) {
     return this.http.post<UsernameAvailableRespone>(
       this.rootUrl + '/auth/username',
       {
-        username
+        username,
       }
     );
   }
 
   signup(credentials: SignupCredentials) {
-    return this.http.post<SignupResponse>(
-      this.rootUrl + '/auth/signup',
-      credentials
-    ).pipe(
-      // Reminder: if we have an error coming out of the request, it's going to skip the tap operator
-      // which is what we want.
-      tap(()=> {
-        this.signedin$.next(true);
+    return this.http
+      .post<SignupResponse>(this.rootUrl + '/auth/signup', credentials, {
+        withCredentials: true
       })
-    )
+      .pipe(
+        // Reminder: if we have an error coming out of the request, it's going to skip the tap operator
+        // which is what we want.
+        tap(() => {
+          this.signedin$.next(true);
+        })
+      );
   }
 
-checkAuth(){
-  return this.http.get(
-    this.rootUrl + '/auth/signedin'
-  ).pipe(
-    tap((response) => {
-      console.log(response);
-    })
-  );
-}
-
+  checkAuth() {
+    return this.http.get(this.rootUrl + '/auth/signedin', { withCredentials:true}).pipe(
+      tap((response) => {
+        console.log(response);
+      })
+    );
+  }
 }
