20cf7f3fab61f1647157a513aeb91d901dda9912
SoyUnAlumno <tomaspraga@hotmail.es>
2022-06-09 12:32:57 +0200

    handle errors when trying to get non existing email

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -2,19 +2,27 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   Resolve,
-  RouterStateSnapshot,
+  RouterStateSnapshot, Router
 } from '@angular/router';
 import { Email } from '../interfaces/Email';
 import { EmailService } from './email.service';
+import { catchError } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmailResolverService implements Resolve<Email> {
-  constructor(private emailService: EmailService) {}
+  constructor(private emailService: EmailService, private router: Router) {}
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const { id } = route.params;
-    return this.emailService.getEmail(id);
+    return this.emailService.getEmail(id).pipe(
+      catchError((err) => {
+        this.router.navigateByUrl('/inbox/not-found');
+        // Return observable only because catchError requires it
+        return EMPTY;
+      })
+    );
   }
 }
